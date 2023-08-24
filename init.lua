86a5aa458ca23e5fe461c0620b6423238b900714
jm <jmlombardi@dukarevich.com.ar>
2023-08-24 20:05:46 -0300

    commit

--- init.lua
+++ init.lua
@@ -194,6 +194,10 @@ require('lazy').setup({
   {
     'mhartington/formatter.nvim',
   },
+
+  {
+    'jwalton512/vim-blade'
+  },
   -- NOTE: Next Step on Your Neovim Journey: Add/Configure additional "plugins" for kickstart
   --       These are some example plugins that I've included in the kickstart repository.
   --       Uncomment any of the lines below to enable them.
